ae74895f158e3db5f4b612c14f487bf7cf6c15e2
Singlerr <singlerr@naver.com>
2019-12-26 00:16:59 +0900

    First

---
+++ Chemistry/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Chemistry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.metroPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(23, 63);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(625, 23);
+            this.input.TabIndex = 0;
+            this.input.Text = "metroTextBox1";
+            this.input.Click += new System.EventHandler(this.metroTextBox1_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 89);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(81, 19);
+            this.metroLabel1.TabIndex = 1;
+            this.metroLabel1.Text = "metroLabel1";
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.comboBox1);
+            this.metroPanel1.Controls.Add(this.metroButton1);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(448, 92);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(200, 100);
+            this.metroPanel1.TabIndex = 2;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(134, 75);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(63, 22);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "OK";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 463);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.input);
+            this.Name = "Form1";
+            this.Text = "Chemistry";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metroPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox input;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+    }
+}
+
---
+++ Chemistry/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chemistry
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        //([0-9][a-zA-Z])* -> ([0-9][a-zA-Z])*
+       
+        public static string reg = @"([0-9][a-zA-Z]) \+? ([0-9][a-zA-Z])* -> ([0-9][a-zA-Z])*";
+        public static Regex regex;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            input.TextChanged += metroTextBox1_TextChanged;
+          regex = new Regex(reg);
+        }
+        private void metroTextBox1_TextChanged(object sender,EventArgs e)
+        {
+            string t = input.Text;
+            if (regex.Match(t).Success)
+            {
+                
+                metroPanel1.Show();
+            }
+        }
+        private void metroTextBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
